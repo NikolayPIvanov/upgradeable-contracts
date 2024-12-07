04e3b5f63167282d2df322d06d380cc560c58877
Nikolay Ivanov <nick.ivanov98@gmail.com>
2024-12-07 08:19:06 +0200

    Adding UUPS

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,8 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract BoxV1 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV1 is UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
@@ -11,4 +22,6 @@ contract BoxV1 {
     function version() external pure returns (uint256) {
         return 1;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal virtual override {}
 }
